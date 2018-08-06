0d3ff92db66856aac8984ee5abb9fde07ece9f1a
PedroRSM <pedro_moreira7@hotmail.com>
2018-08-06 12:08:05 +0100

    -feed news done -new details a funcionar

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,9 @@ import 'dart:async';
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_html_view/flutter_html_view.dart';
 import 'package:http/http.dart' as http;
+
 
 void main() {
   runApp(MaterialApp(
@@ -81,12 +83,23 @@ class WeMysticNewsState extends State<WeMysticNewsData> {
                             new FlatButton(
                               child: const Text('SHARE'),
                               textColor: Colors.amber.shade500,
-                              onPressed: () { /* do nothing */ },
+                              onPressed: () {
+
+                              },
                             ),
                             new FlatButton(
                               child: const Text('EXPLORE'),
                               textColor: Colors.amber.shade500,
-                              onPressed: () { /* do nothing */ },
+                              onPressed: () {
+                                var route = new MaterialPageRoute(
+                                  builder: (BuildContext context) =>
+                                  new NewDetail(
+                                      value: data[index]["content"].toString(),
+                                      thumbnail: data[index]["thumbnail"].toString()),
+
+                                );
+                                Navigator.of(context).push(route);
+                              },
                             ),
                           ],
                         ),
@@ -124,3 +137,39 @@ class WeMysticNewsState extends State<WeMysticNewsData> {
     this.getWMNews();
   }
 }
+
+class NewDetail extends StatefulWidget {
+  final String value;
+  final String thumbnail;
+  NewDetail({Key key, this.value, this.thumbnail}) : super (key: key);
+
+  @override
+  _NewDetailState createState() => _NewDetailState();
+}
+
+class _NewDetailState extends State<NewDetail> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        appBar: new AppBar(
+          title: new Text("test"),
+        ),
+        body: new SingleChildScrollView(
+          child: new Center(
+            child: new Column(
+              children: <Widget>[
+                new Image.network("${widget.thumbnail}"),
+                new HtmlView(data: "${widget.value}"),
+
+              ],
+            ),
+
+          ),
+        )
+
+    );
+  }
+}
+
+
+
